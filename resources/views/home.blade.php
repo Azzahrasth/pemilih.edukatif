2e96293b1770f3d138f4efdbc4d0ee314fe951a0
hafidzf25 <abdullahnestax@gmail.com>
2024-08-18 15:48:10 +0700

    Beranda done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -47,25 +47,25 @@
 </div>
 
 <div class="row p-3 mx-5">
-    <div class="col-12 poppins-extrabold" style="font-size: 50px;">Apa itu Pilkada?</div>
+    <div class="col-12 poppins-extrabold text-lg-start text-center" style="font-size: 50px;">Apa itu Pilkada?</div>
 </div>
 
 <div class="row p-3 mb-4 mx-5 justify-content-center" style="background-color: #F1F1F1; border-radius: 20px;">
     <div class="col-12 poppins-bold p-5 pb-0 my-4 mb-0 text-center" style="color: #162F50; font-size: 32px;">
         <p>Pemilihan Kepala Daerah yang diadakan untuk <span style="color: #F6B951;">memilih</span> kepala daerah beserta wakilnya di tingkat <span style="color: #F6B951;">provinsi</span> (gubernur), <span style="color: #F6B951;">kota</span> (wali kota), dan <span style="color: #F6B951;">kabupaten</span> (bupati) secara langsung setiap lima tahun sekali. Tahun ini, pilkada akan dilaksanakan serentak di:</p>
     </div>
-    <div class="row justify-content-center">
+    <div class="row justify-content-center mt-3">
         <div class="col-2 poppins-bold text-center me-5" style="background-color: #162F50; font-size: 96px; color: white; border-radius: 25px 25px 0px 0px;">37</div>
-        <div class="col-2 poppins-bold text-center" style="background-color: #162F50; font-size: 96px; color: white; border-radius: 25px 25px 0px 0px;">576</div>
+        <div class="col-2 poppins-bold text-center ms-5" style="background-color: #162F50; font-size: 96px; color: white; border-radius: 25px 25px 0px 0px;">576</div>
     </div>
     <div class="row pb-4 justify-content-center">
         <div class="col-2 poppins-bold text-center me-5" style="background-color: white; font-size: 24px; color: #162f50; border-radius: 0px 0px 25px 25px;">Provinsi</div>
-        <div class="col-2 poppins-bold text-center" style="background-color: white; font-size: 20px; color: #162f50; border-radius: 0px 0px 25px 25px;">Kabupaten/Kota</div>
+        <div class="col-2 poppins-bold text-center ms-5" style="background-color: white; font-size: 20px; color: #162f50; border-radius: 0px 0px 25px 25px;">Kabupaten/Kota</div>
     </div>
 </div>
 
 <div class="row p-3 mb-4 mx-5">
-    <div class="col-12 poppins-extrabold pb-5" style="font-size: 50px;">Tanggal Penting!!</div>
+    <div class="col-12 poppins-extrabold pb-5 text-lg-start text-center" style="font-size: 50px;">Tanggal Penting!!</div>
     <div class="col">
         <div class="timeline-steps aos-init aos-animate" data-aos="fade-up">
             <div class="timeline-step">
@@ -96,6 +96,69 @@
                 </div>
             </div>
         </div>
+    </div>
+</div>
+
+<div class="row p-3 mx-5">
+    <div class="col-12 poppins-extrabold text-lg-start text-center" style="font-size: 50px;">Cara Mencoblos di TPS</div>
+</div>
+
+<div class="row p-3 mb-4 mt-2 mx-5 justify-content-center" style="background-color: #F1F1F1; border-radius: 20px;">
+    <div class="col-12 poppins-bold p-5 pt-3 pb-3 my-2 text-center" style="color: #162F50; font-size: 32px;">
+        <p>Tahukah kamu? Indonesia mempunyai tata cara dalam menggunakan hak suara ketika Pilkada. Sobat pemilih, akan mendapatkan dua surat suara untuk Gubernur dan Bupati atau Walikota. Simak prosedurnya, yuk!</p>
+    </div>
+</div>
+
+<div class="container py-3 poppins-regular">
+    <div class="main-timeline mb-5">
+        <div class="timeline left">
+            <div class="card">
+                <div class="card-body p-4">
+                    <h3 class="poppins-bold">Tahap 1</h3>
+                    <p class="mb-0">Datanglah ke TPS tepat waktu dari pukul 07.00 - 13.00. Petugas KPPS akan mengarahkan untuk mengisi daftar hadir.</p>
+                </div>
+            </div>
+        </div>
+        <div class="timeline right">
+            <div class="card">
+                <div class="card-body p-4">
+                    <h3 class="poppins-bold">Tahap 2</h3>
+                    <p class="mb-0">Sobat Pemilih kemudian akan diminta untuk menyerahkan KTP asli dan Formulir pemberitahuan pemungutan suara untuk dilakukan pengecekan. Setelah selesai, Sobat Pemilih akan menunggu giliran.</p>
+                </div>
+            </div>
+        </div>
+        <div class="timeline left">
+            <div class="card">
+                <div class="card-body p-4">
+                    <h3 class="poppins-bold">Tahap 3</h3>
+                    <p class="mb-0">Setelah dipanggil, Sobat Pemilih akan mendapatkan dua surat suara. Yakni surat suara Calon Gubernur (warna) dan Calon Bupati/Walikota (warna).</p>
+                </div>
+            </div>
+        </div>
+        <div class="timeline right">
+            <div class="card">
+                <div class="card-body p-4">
+                    <h3 class="poppins-bold">Tahap 4</h3>
+                    <p class="mb-0">Pergi ke bilik suara untuk melakukan pencoblosan. Setelah kedua surat suara dicoblos, Sobat Pemilih dapat melipat kembai surat suara sesuai petunjuk dan memasukkannya ke dalam kotak suara.</p>
+                </div>
+            </div>
+        </div>
+        <div class="timeline left">
+            <div class="card">
+                <div class="card-body p-4">
+                    <h3 class="poppins-bold">Tahap 5</h3>
+                    <p class="mb-0">Terakhir, celupkan salah satu ujung jari tangan ke tinta sebagai bukti telah menggunakan hak suara dalam Pilkada.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row text-center mt-lg-4 mt-1 mb-4">
+    <div class="col-12 poppins-extrabold mb-4" style="font-size: 80px;">🌐 QUIZ 🌐</div>
+    <div class="col-12 poppins-extrabold mb-4" style="font-size: 32px;">Untuk mengukur tingkat pemahamanmu mengenai Pilkada.</div>
+    <div class="col-lg-12 mb-4">
+        <a href="#" class="btn poppins-bold px-4" style="background-color: #F6B951; color: white; font-size: 48px; border-radius: 15px;">Cobain, yuk!</a>
     </div>
 </div>
 
