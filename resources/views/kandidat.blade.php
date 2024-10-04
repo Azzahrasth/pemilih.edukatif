4ae6bbcefe89d2822d170c08e11342337d4cc2a7
Azzahrasth <azzahrasth@upi.edu>
2024-10-04 12:24:35 +0700

    gambar kandidat

--- resources/views/kandidat.blade.php
+++ resources/views/kandidat.blade.php
@@ -48,8 +48,8 @@
     @foreach($kandidats as $kandidat)
     <div class="col-lg-4 col-12 pt-3 mb-3 deskripsi-daftar-partai d-flex justify-content-center">
         <div class="card p-3" style="box-shadow: 0 5px 15px rgba(0, 0, 0, 0.1); border-radius: 8px; width: 100%; max-width: 300px;">
-            <div style="width: 100%; height: 260px; overflow: hidden; border-radius: 8px;">
-                <img class="card-img-top mx-auto" src="{{ $kandidat->image }}" alt="Card image cap" style="width: 100%; height: 100%; object-fit: cover;">
+            <div style="width: 100%; height: 260px; overflow: hidden; border-radius: 8px; display: flex; align-items: center;">
+                <img class="card-img-top mx-auto" src="{{ $kandidat->image }}" alt="Card image cap" style="width: 100%; height: auto; object-fit: cover;">
             </div>
             <div class="card-body d-flex flex-column justify-content-center align-items-center">
                 <h5 class="card-title blue text-center poppins-bold" style="font-size: 18px; text-align: center; margin: 0 auto;">{{ $kandidat->nama }}</h5>
